a2a884be968e76f6281488026c304dd44162eda5
thaynewp <twpeterson1993@gmail.com>
2015-10-22 12:08:55 -0600

    Calculations changes

--- YouGotNoCake/test/byui/cit260/youGotNoCake/control/CalculationsControlTest.java
+++ YouGotNoCake/test/byui/cit260/youGotNoCake/control/CalculationsControlTest.java
@@ -57,6 +57,136 @@ public class CalculationsControlTest {
         
        }  
     @Test
+    public void test2CalcGallons() {
+        
+        /****************************
+         * * Test case #2 CalcGallons
+         * **************************/
+        System.out.println("calcGallons");
+        System.out.println("\tTest case #2");
+        
+        // input values for test case 1
+        double numFamilyMbrs = 5;
+                
+        // expected output return value
+        double expGallons = 87.5;
+                
+        //create instance of CalcGallons class
+        CalculationsControl instance = new CalculationsControl();
+        
+        // call function to run test
+        double gallons = instance.calcGallons(numFamilyMbrs);
+        
+        // compare expected return value with actual value returned
+        // may need to search web-- JUnit JavaDoc Assert
+        assertEquals(expGallons, gallons, 0.0001);
+        
+       }  
+    @Test
+    public void test3CalcGallons() {
+        
+        /****************************
+         * * Test case #3 CalcGallons
+         * **************************/
+        System.out.println("calcGallons");
+        System.out.println("\tTest case #3");
+        
+        // input values for test case 1
+        double numFamilyMbrs = 3;
+                
+        // expected output return value
+        double expGallons = 52.5;
+                
+        //create instance of CalcGallons class
+        CalculationsControl instance = new CalculationsControl();
+        
+        // call function to run test
+        double gallons = instance.calcGallons(numFamilyMbrs);
+        
+        // compare expected return value with actual value returned
+        // may need to search web-- JUnit JavaDoc Assert
+        assertEquals(expGallons, gallons, 0.0001);
+        
+       }  
+    @Test
+    public void test4CalcGallons() {
+        
+        /****************************
+         * * Test case #4 CalcGallons
+         * **************************/
+        System.out.println("calcGallons");
+        System.out.println("\tTest case #4");
+        
+        // input values for test case 1
+        double numFamilyMbrs = aa;
+                
+        // expected output return value
+        double expGallons = error;
+                
+        //create instance of CalcGallons class
+        CalculationsControl instance = new CalculationsControl();
+        
+        // call function to run test
+        double gallons = instance.calcGallons(numFamilyMbrs);
+        
+        // compare expected return value with actual value returned
+        // may need to search web-- JUnit JavaDoc Assert
+        assertEquals(expGallons, gallons, 0.0001);
+        
+       }  
+    @Test
+    public void test5CalcGallons() {
+        
+        /****************************
+         * * Test case #5 CalcGallons
+         * **************************/
+        System.out.println("calcGallons");
+        System.out.println("\tTest case #5");
+        
+        // input values for test case 1
+        double numFamilyMbrs = &@;
+                
+        // expected output return value
+        double expGallons = error;
+                
+        //create instance of CalcGallons class
+        CalculationsControl instance = new CalculationsControl();
+        
+        // call function to run test
+        double gallons = instance.calcGallons(numFamilyMbrs);
+        
+        // compare expected return value with actual value returned
+        // may need to search web-- JUnit JavaDoc Assert
+        assertEquals(expGallons, gallons, 0.0001);
+        
+       }  
+    @Test
+    public void test6CalcGallons() {
+        
+        /****************************
+         * * Test case #6 CalcGallons
+         * **************************/
+        System.out.println("calcGallons");
+        System.out.println("\tTest case #6");
+        
+        // input values for test case 1
+        double numFamilyMbrs = null;
+                
+        // expected output return value
+        double expGallons = error;
+                
+        //create instance of CalcGallons class
+        CalculationsControl instance = new CalculationsControl();
+        
+        // call function to run test
+        double gallons = instance.calcGallons(numFamilyMbrs);
+        
+        // compare expected return value with actual value returned
+        // may need to search web-- JUnit JavaDoc Assert
+        assertEquals(expGallons, gallons, 0.0001);
+        
+       }  
+    @Test
     public void test1CalcBMI() {
         
         /*************************
