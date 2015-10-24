3bec99dfd1ec4d635b980a667dbb0ed256236c1d
Betsey Delorey <byuideloreyb@gmail.com>
2015-10-24 11:23:29 -0500

    Betsey Test Cases

--- YouGotNoCake/test/byui/cit260/youGotNoCake/control/CalculationsControlTest.java
+++ YouGotNoCake/test/byui/cit260/youGotNoCake/control/CalculationsControlTest.java
@@ -30,6 +30,7 @@ public class CalculationsControlTest {
     /**
      * Test of calcGallons method, of class CalculationsControl.
      */
+       
     @Test
     public void test1CalcGallons() {
         
