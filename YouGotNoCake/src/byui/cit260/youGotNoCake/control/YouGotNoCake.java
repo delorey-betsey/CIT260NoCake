edcbc16536ef12dbe04f38ccb88f1c99ad9076ba
Betsey Delorey <byuideloreyb@gmail.com>
2015-10-29 16:47:28 -0500

    Removing Design Docs

---
+++ YouGotNoCake/src/byui/cit260/youGotNoCake/control/YouGotNoCake.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.youGotNoCake.control;
+
+import byui.cit260.youGotNoCake.view.StartProgramView;
+
+/**
+ *
+ * @author Betsey
+ */
+public class YouGotNoCake {
+     
+    public static void main(String[] args){
+        //create Start ProgramView and start the program
+        StartProgramView startProgamView = new StartProgramView();
+        startProgramView.startProgram();
+    }
+}
---
+++ YouGotNoCake/src/byui/cit260/youGotNoCake/view/StartProgramView.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.youGotNoCake.view;
+
+/**
+ *
+ * @author Betsey
+ */
+public class StartProgramView {
+    
+    public StartProgramView() {
+    
+    public void startProgram(){
+        //Display the banner screen
+        this.displayBanner();
+        //prompt the player to enter their name Retrieve the name of the player
+        //Create and save the player object
+        //Display a personalized welcome message
+        //Display the Main menu.
+        
+    }
+
+    public void displayBanner() {
+        //To change body of generated methods, choose Tools | Templates.
+        System.out.println("*\n Game description and how to play. *");
+        
+        System.out.println("*\n This is a test. *");
+        
+        System.out.println("*\n This is a test. *");
+        
+        System.out.println("*\n ************************* *");
+        
+    }
+}
