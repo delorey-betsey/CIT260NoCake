accbd47857f3d31aefb73d774fd5d1400bd10bdf
Betsey Delorey <byuideloreyb@gmail.com>
2015-11-03 10:36:37 -0600

    Betsey adding Help Menu text.

--- YouGotNoCake/src/byui/cit260/youGotNoCake/view/HelpMenuView.java
+++ YouGotNoCake/src/byui/cit260/youGotNoCake/view/HelpMenuView.java
@@ -19,7 +19,7 @@ class HelpMenuView {
                 +"\n----------------------------------"
                 +"\nG - What is the goal of the game?"
                 +"\nM - How to move."
-                +"\nS - Store list items."
+                +"\nC - Items on shopping lists."
                 +"\nE - Exit"
                 +"\n----------------------------------";
     
@@ -59,7 +59,7 @@ private String getInput() {
             }else{
             if("G".equals(selection) ||
                 "M".equals(selection) ||
-                "S".equals(selection) ||
+                "C".equals(selection) ||
                 "E".equals(selection) ){
                 break; //out of the repetition (exit)
             }else
@@ -70,14 +70,14 @@ private String getInput() {
     }
     public void doAction(char selection){
         switch (selection){
-            case 'G': //get and start existing game
+            case 'G': //display goal of game
                 this.displayGoal();
                 break;
-            case 'M': //display the help menu
+            case 'M': //display how to move
                 this.displayMove();
                 break;
-            case 'S': //save the current game
-                this.displayStore();
+            case 'C': //display items on shopping lists
+                this.displayCrossOff();
                 break;
             case 'E': //exit the program
                 return;
@@ -86,12 +86,52 @@ private String getInput() {
         }
     }
     private void displayGoal() {
-        System.out.println("*** displayGoal function called ***");
-    }
+        System.out.println("*\nThe object of the game is to find cake.  You can "
+                + "\nget lucky and find cake in two of the 25 locations in the town.  "
+                + "\nAutomatic win.  Otherwise, you can win if you find all four "
+                + "\nitems on any of the three lists.  Twelve of the 25 locations "
+                + "\nhave various items on the player’s shopping lists.  This too "
+                + "\nresults in automatically winning the grand cake prize…and your "
+                + "\ngame is over. Be careful out there.  You can also get unlucky"
+                + "\nand automatially die! The goal of the game is happy, scarey surprises.");
+        System.out.println("*\n ****************************************************** *");                       
+        }
+    
     private void displayMove() {
-        System.out.println("*** displayMove function called ***");
+        System.out.println("*\nYou play this game by entering input on the screen "
+                + "\nand hitting 'Enter'. Follow the prompts to enter menu choices "
+                + "\nto go to a new location or cross off items on your shopping "
+                + "\nlists.  The program will also ask for player info to make"
+                + "\nthree calculations.");
+        
+        System.out.println("*\nAlong your way, the game will offer to calculate   "
+                + "\nyour BMI, calculate your days until Christmas, and calculate "
+                + "\nthe gallons of water your household flushes each day, all based "
+                + "\nupon player input.");
+        
+        System.out.println("*\n ****************************************************** *"); 
     }
-    private void displayStore() {
-        System.out.println("*** displayStore function called ***"); 
-    }
+    private void displayCrossOff() {
+        System.out.println("*\nAs you are going around town, you figure, why not shop?"
+                + "\nYou are mad about shopping.  You want to find things to make"
+                + "\na cake, things for a birthday party and a variety "
+                + "\nof sleep aids.  You haven’t been sleeping well lately, obviously, "
+                + "\nwaking up in a closet.");
+        System.out.println("*\nCake ingredients:" 
+                +"\n1.	Flour" 
+                +"\n2.	Sugar" 
+                +"\n3.	Butta" 
+                +"\n4.	Bananas");
+        
+        System.out.println("*\nBirthday party supply list:" 
+                +"\n1.	Whoopie cushion" 
+                +"\n2.	5 gallons of ice cream" 
+                +"\n3.	Grim reaper" 
+                +"\n4.	Plastic forks");
+        
+        System.out.println("*\nSleep-aids:" 
+                +"\n1.	Apnea Fairy" 
+                +"\n2.	Tranquilizer dart" 
+                +"\n3.	Tax code" 
+                +"\n4.	Warm milk");}
 }
